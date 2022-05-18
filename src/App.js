3b59164c1216e2922625f4799eff563d31dcab70
Horacio Munoz Ramirez <horacio@Horacios-MacBook-Pro.local>
2022-05-18 13:11:34 -0700

    Completed Part 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import './App.css';
+import React from "react";
+import TodoList from "./TodoList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <TodoList />;
 }
 
 export default App;
